6a78ebaf89b28cc5ef22cdc83262c9d7fe821063
Alex Lee <hyunho98@gmail.com>
2020-07-29 07:43:43 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:user][:name])
-    user = user.try(:authenticate, params[:user][:password])
+    @user = User.find_by(name: params[:user][:name])
+    @user = user.try(:authenticate, params[:user][:password])
 
     return redirect_to controller: 'sessions', action: 'new') unless user
 
-    session[:user_id] = user.id
-    @user = user
+    session[:user_id] = @user.id
 
 end
