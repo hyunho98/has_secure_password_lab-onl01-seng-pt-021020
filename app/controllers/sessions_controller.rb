01118720436d7dad5e8b7752752cf6d02f2dc6f8
Alex Lee <hyunho98@gmail.com>
2020-07-29 07:44:47 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,4 +13,8 @@ class SessionsController < ApplicationController
     redirect_to controller: 'welcome', action: 'home'
   end
 
+  def destroy
+    session.delete :user_id
+    redirect_to '/'
+  
 end
